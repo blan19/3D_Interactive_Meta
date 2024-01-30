a54fc1d19ed190c918264469c63c8dd47383c559
blan19 <oponize@naver.com>
2024-01-30 19:19:56 +0900

    feat(server): init app path

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { RouterModule } from "@nestjs/core";
+import { ChatModule } from "./chat/chat.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    RouterModule.register([
+      {
+        path: "chat",
+        module: ChatModule,
+      },
+    ]),
+  ],
 })
 export class AppModule {}
