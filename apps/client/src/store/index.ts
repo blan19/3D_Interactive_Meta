0630d2d1c95aa4401479c452bae118f7456495b7
blan19 <oponize@naver.com>
2024-02-04 22:26:14 +0900

    feat(client): add user and character store

--- apps/client/src/store/index.ts
+++ apps/client/src/store/index.ts
@@ -5,9 +5,30 @@ interface AvatarState {
   createAvatar: (url: string) => void;
 }
 
+interface UserState {
+  id: string;
+  updateId: (url: string) => void;
+}
+
+interface CharactersState {
+  characters: any[];
+  updateCharacters: (characters: any) => void;
+}
+
 const useAvatarStore = create<AvatarState>((set) => ({
   url: null,
   createAvatar: (newUrl: string) => set(() => ({ url: newUrl })),
 }));
 
-export { useAvatarStore };
+const useUserStore = create<UserState>((set) => ({
+  id: '',
+  updateId: (newId: string) => set(() => ({ id: newId })),
+}));
+
+const useCharactersStore = create<CharactersState>((set) => ({
+  characters: [],
+  updateCharacters: (newCharacters: any) =>
+    set((state) => ({ characters: [...state.characters, newCharacters] })),
+}));
+
+export { useAvatarStore, useUserStore, useCharactersStore };
