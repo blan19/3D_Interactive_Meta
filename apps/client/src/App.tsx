17858a82b678830c6ec8edc4bae285419cd664f1
blan19 <oponize@naver.com>
2024-02-04 22:27:05 +0900

    feat(client): apply keyboard event setting

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,18 +1,45 @@
+import { useMemo } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Scene from './components/scene';
 import OnBoarding from './components/ui/onboarding';
 import useLoaded from './hooks/useLoaded';
 import Loading from './components/loading';
+import {
+  KeyboardControls,
+  type KeyboardControlsEntry,
+} from '@react-three/drei';
+import { KeyBoardManager } from './components/manager';
+
+export enum Controls {
+  forward = 'forward',
+  back = 'back',
+  left = 'left',
+  right = 'right',
+  jump = 'jump',
+}
 
 function App() {
   const { loaded } = useLoaded();
+  const map = useMemo<KeyboardControlsEntry<Controls>[]>(
+    () => [
+      { name: Controls.forward, keys: ['ArrowUp', 'KeyW'] },
+      { name: Controls.back, keys: ['ArrowDown', 'KeyS'] },
+      { name: Controls.left, keys: ['ArrowLeft', 'KeyA'] },
+      { name: Controls.right, keys: ['ArrowRight', 'KeyD'] },
+      { name: Controls.jump, keys: ['Space'] },
+    ],
+    []
+  );
 
   return (
     <main className="h-full w-full">
       <OnBoarding />
-      <Canvas>
-        <Scene />
-      </Canvas>
+      <KeyboardControls map={map}>
+        <Canvas>
+          <KeyBoardManager />
+          <Scene />
+        </Canvas>
+      </KeyboardControls>
       {!loaded && <Loading />}
     </main>
   );
