0164c9aaf10a3fa5fe4f83b919daa48dcd7870a0
blan19 <oponize@naver.com>
2024-03-03 12:58:44 +0900

    chore(client): withCredentials options true

--- apps/client/src/lib/socket.ts
+++ apps/client/src/lib/socket.ts
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 import { config } from './config';
 
-const socket = io(`${config.env.VITE_PUBLIC_SERVER_URL}`);
+const socket = io(`${config.env.VITE_PUBLIC_SERVER_URL}`, {
+  withCredentials: true,
+});
 
 export { socket };
